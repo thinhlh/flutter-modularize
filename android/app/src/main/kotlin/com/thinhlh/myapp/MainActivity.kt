2c8cd1324767b6f7c2dace29701092d27731f8e5
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-08-17 14:12:09 +0700

    [FEAT] initialize app

---
+++ android/app/src/main/kotlin/com/thinhlh/myapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thinhlh.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ libraries/lib1/lib/lib1.dart
@@ -0,0 +1,9 @@
+library lib1;
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
